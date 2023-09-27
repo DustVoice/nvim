4a1df4edcd405b991facdb9f0c5f6e699aaffdfc
DustVoice <info@dustvoice.de>
2023-09-27 08:52:22 +0200

    Remove small mistake

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local nvchad = vim.api.nvim_get_runtime_file("NvChad", false)[1]
 
 if nvchad then
-  package.path = nvchad .. ';' .. nvchad .. '/lua/?/init.lua;' .. nvchad .. '/lua/?.lua;' .. package.path
+  package.path = nvchad .. '/lua/?/init.lua;' .. nvchad .. '/lua/?.lua;' .. package.path
   dofile(nvchad .. "/init.lua")
 end
