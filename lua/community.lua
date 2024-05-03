c5464fa782b6e34cbf5ce1c44d8e7df2bab77597
DustVoice <info@dustvoice.de>
2024-05-03 10:51:42 +0200

    Switch to ruff

--- lua/community.lua
+++ lua/community.lua
@@ -22,7 +22,7 @@ return {
   -- { import = "astrocommunity.pack.haskell" },
   { import = "astrocommunity.pack.julia" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typst" },
