6f26d5fed2a30e5360e7e66bf23b34eeadb79961
DustVoice <info@dustvoice.de>
2024-11-13 22:10:29 +0100

    Switch to Iosevka

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 -- Setup Neovide Font
-if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h13"
-end
+if vim.g.neovide then vim.o.guifont = "IosevkaTerm Nerd Font:h13" end
 
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
